24906aa00150990b909277fcc3effb44d2d6aa80
Haruna <pa.runa.na12_miley@gmail.com>
2021-06-15 21:58:18 +0900

    レスポンシブ対応更新

---
+++ wp-content/themes/ojt/app.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.img-wrap img:nth-child(n+2)').hide();
+    setInterval(function() {
+    $(".img-wrap img:first-child").fadeOut(3000);
+    $(".img-wrap img:nth-child(2)").fadeIn(3000);
+    $(".img-wrap img:first-child").appendTo(".img-wrap");
+    }, 3000);
+});
