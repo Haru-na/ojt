283c6313bebd0bf268081ffc5dfcab5918f00e2d
TakakoTequilaKimura <k.takako33@gmail.com>
2021-06-16 12:17:09 +0900

    burgermenuのボタンだけいったん

--- wp-content/themes/ojt/app.js
+++ wp-content/themes/ojt/app.js
@@ -1,8 +1,15 @@
-$(function(){
+$(function () {
     $('.img-wrap img:nth-child(n+2)').hide();
-    setInterval(function() {
-    $(".img-wrap img:first-child").fadeOut(3000);
-    $(".img-wrap img:nth-child(2)").fadeIn(3000);
-    $(".img-wrap img:first-child").appendTo(".img-wrap");
+    setInterval(function () {
+        $(".img-wrap img:first-child").fadeOut(3000);
+        $(".img-wrap img:nth-child(2)").fadeIn(3000);
+        $(".img-wrap img:first-child").appendTo(".img-wrap");
     }, 3000);
+
+    // ハンバーガー用
+    $('.hamburger').on('click', function(){
+        $(".burger-btn").toggleClass("close");　//closeというclassを追加/削除
+        $(".menu-content").fadeToggle(500);  //0.5秒かけてフェードイン/アウト
+    });
+
 });
