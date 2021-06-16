8af1d4086b3d166d3b9a634a41f9b9cc3011d2e2
TakakoTequilaKimura <k.takako33@gmail.com>
2021-06-16 14:25:47 +0900

    ハンバーガーCSSだけで、できたぁ

--- wp-content/themes/ojt/app.js
+++ wp-content/themes/ojt/app.js
@@ -6,10 +6,5 @@ $(function () {
         $(".img-wrap img:first-child").appendTo(".img-wrap");
     }, 3000);
 
-    // ハンバーガー用
-    $('.hamburger').on('click', function(){
-        $(".burger-btn").toggleClass("close");　//closeというclassを追加/削除
-        $(".menu-content").slideToggle(500);  //0.5秒かけてフェードイン/アウト
-    });
 
 });
