da714ab71c77a2afc3c326ff92415b8ae9f3de32
TakakoTequilaKimura <k.takako33@gmail.com>
2021-06-17 14:40:33 +0900

    WPのjs実装完了

--- wp-content/themes/ojt/footer.php
+++ wp-content/themes/ojt/footer.php
@@ -3,7 +3,8 @@
         </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="./app.js"></script>
+    <!-- <script src="./app.js"></script> -->
+    <?php wp_footer();?>
 </body>
 
 </html>
--- wp-content/themes/ojt/functions.php
+++ wp-content/themes/ojt/functions.php
@@ -16,7 +16,7 @@ function ojt_script(){
     wp_enqueue_style( 'style', get_template_directory_uri() . '/css/ojt.css', array(), '1.0.0' );
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
     // js読み込み
-    wp_enqueue_script( 'script-name', get_template_directory_uri() . 'js/app.js', array(), '1.0.0',true);
+    wp_enqueue_script( 'ojt', get_template_directory_uri() . '/app.js', array(), '1.0.0',true);
 
 }
 add_action( 'wp_enqueue_scripts', 'ojt_script' );
