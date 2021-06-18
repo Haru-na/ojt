7492320404ece866c4715722f17a989adb928e1d
TakakoTequilaKimura <k.takako33@gmail.com>
2021-06-18 15:05:27 +0900

    もっとをもっとしたい

--- wp-content/themes/ojt/index.php
+++ wp-content/themes/ojt/index.php
@@ -7,42 +7,11 @@
             </picture> -->
              <!-- スライダー -->
              <div class="slider_pc">
-                <?php echo do_shortcode('[smartslider3 slider="4"]');?>
+             <?php echo do_shortcode('[smartslider3 slider="2"]');?>
              </div>
              <div class="slider_sp">
-                <?php echo do_shortcode('[smartslider3 slider="5"]');?>
+             <?php echo do_shortcode('[smartslider3 slider="3"]');?>
              </div>
-
-            <!-- <div class="img-wrap">
-                <picture class="main-img">
-                    <source media="(min-width: 768px)" srcset="<hp echo get_template_directory_uri();?>/img/visual_1_pc.png">
-                    <img src="<hp echo get_template_directory_uri();?>/img/visual_1_sp.png" alt=""> 
-                </picture>
-                <picture class="main-img">
-                    <source media="(min-width: 768px)" srcset="<hp echo get_template_directory_uri();?>/img/visual_2_pc.png">
-                    <img src="<hp echo get_template_directory_uri();?>/img/visual_2_sp.png" alt=""> 
-                </picture>
-                <picture class="main-img">
-                    <source media="(min-width: 768px)" srcset="<hp echo get_template_directory_uri();?>/img/visual_3_pc.png">
-                    <img src="<hp echo get_template_directory_uri();?>/img/visual_3_sp.png" alt=""> 
-                </picture>
-                <picture class="main-img">
-                    <source media="(min-width: 768px)" srcset="<p echo get_template_directory_uri();?>/img/visual_4_pc.png">
-                    <img src="<hp echo get_template_directory_uri();?>/img/visual_4_sp.png" alt=""> 
-                </picture>
-                <picture class="main-img">
-                    <source media="(min-width: 768px)" srcset="<hp echo get_template_directory_uri();?>/img/visual_5_pc.png">
-                    <img src="<hp echo get_template_directory_uri();?>/img/visual_5_sp.png" alt=""> 
-                </picture>
-                <picture class="main-img">
-                    <source media="(min-width: 768px)" srcset="<hp echo get_template_directory_uri();?>/img/visual_6_pc.png">
-                    <img src="<hp echo get_template_directory_uri();?>/img/visual_6_sp.png" alt=""> 
-                </picture>
-                <picture class="main-img">
-                    <source media="(min-width: 768px)" srcset="<hp echo get_template_directory_uri();?>/img/visual_7_pc.png">
-                    <img src="<hp echo get_template_directory_uri();?>/img/visual_7_sp.png" alt=""> 
-                </picture>
-            </div> -->
 
             <picture class="main-text">
                 <source media="(min-width: 768px)" srcset="<?php echo get_template_directory_uri();?>/img/visual_text_pc.png">          
@@ -61,7 +30,7 @@
         <div class="post-blogBox">
         <?php
              if( have_posts() ) :
-            while( have_posts() ) :
+            while( have_posts()):
             the_post(); ?> <!--この下からendwhile;までがループ処理中-->
             <div class="post-blogBox">
                 <ul class="post-blogs__meta">
@@ -79,11 +48,11 @@
             </div>
 <!-- ここからもっと見る対応したい -->
 
-
         <?php endwhile;
     else:
         ?><p>表示する記事がありません</p><?php
     endif;
     ?></div>
+    <!-- <div class="more_btn"><p></p></div> -->
 </div><!--class=bodyの閉じタグ-->
 <?php get_footer(); ?>
